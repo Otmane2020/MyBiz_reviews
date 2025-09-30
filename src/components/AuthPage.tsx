169d83aeeb281bae5c6fe9a7fedb0671471a8d67
Otmane2020 <61626050+Otmane2020@users.noreply.github.com>
2025-09-30 10:21:39 +0200

    Updated AuthPage.tsx

--- src/components/AuthPage.tsx
+++ src/components/AuthPage.tsx
@@ -35,7 +35,7 @@ const AuthPage: React.FC<AuthPageProps> = ({ onGoogleAuth, onEmailAuth }) => {
             access_type: 'offline',
             prompt: 'consent',
           },
-          scopes: 'https://www.googleapis.com/auth/business.manage https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/business.manage'
+          scopes: 'https://www.googleapis.com/auth/business.manage https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email'
         }
       });
     } catch (error) {
